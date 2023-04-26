0277403b62ec2215d7b28a0d58c235d9d1505885
张猛 <zhangmeng717@zzbank.cn>
2023-04-26 15:47:43 +0800

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x0009833dFafABe95d847496425AF5fA38cE83Bd4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
